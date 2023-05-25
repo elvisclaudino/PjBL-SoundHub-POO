6c08852713fefa481e832ff331f122f8b1a3bff2
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-05-25 20:33:52 -0300

    update

--- src/Add.java
+++ src/Add.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public interface Add {
    void adicionarMusica(Musica musica);
    ArrayList<Musica> obterListaMusicas();
-
    ArrayList<Album> obterAlbumMusicas();
    String getNome();
 }
