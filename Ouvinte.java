9e5925d06953656677faf7190d47cd308fe7cfca
Cauã de França Brito <102918321+CauaBrito01@users.noreply.github.com>
2023-05-25 14:12:34 -0300

    add classe de ouvinte
    add classe de ouvinte

---
+++ Ouvinte.java
@@ -0,0 +1,14 @@
+public class Ouvinte {
+    private String plano;
+
+    public void plano() {
+        this.plano = plano;
+    }
+
+    public String getPlano() {
+        return plano;
+    }
+
+    
+
+}
