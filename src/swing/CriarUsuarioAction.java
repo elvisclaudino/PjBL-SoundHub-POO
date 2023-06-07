a65effd925b338ba4bb472d6f544abf974621866
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-06-06 22:31:55 -0300

    [updae] menu

--- src/swing/CriarUsuarioAction.java
+++ src/swing/CriarUsuarioAction.java
@@ -7,13 +7,11 @@ import java.awt.event.ActionListener;
 public class CriarUsuarioAction implements ActionListener {
 
     private JTextField catchNome; // cria o atributo texto que posteriormente vai captar o usuario
-    private JComboBox catchTipoUser;
     private JComboBox catchSexo;
     private JTextField catchIdade;
     private JTextField catchNacionalidade;
-    public CriarUsuarioAction(JTextField catchNome, JComboBox catchTipoUser, JComboBox catchSexo, JTextField catchIdade, JTextField catchNacionalidade){ //construtor
+    public CriarUsuarioAction(JTextField catchNome, JComboBox catchSexo, JTextField catchIdade, JTextField catchNacionalidade){ //construtor
         this.catchNome = catchNome;
-        this.catchTipoUser = catchTipoUser;
         this.catchSexo = catchSexo;
         this.catchIdade = catchIdade;
         this.catchNacionalidade = catchNacionalidade;
@@ -21,7 +19,6 @@ public class CriarUsuarioAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e){
         String nome = catchNome.getText(); // GET recebe o valor do input
-        String tipoUser= (String)catchTipoUser.getSelectedItem();
         String sexo = (String)catchSexo.getSelectedItem();
         String idade = catchIdade.getText();
         String nacionalidade = catchNacionalidade.getText();
