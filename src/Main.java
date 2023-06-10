03b333120011755621cb88a22bc43f9487200d0c
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-06-09 23:51:30 -0300

    [ADD] central painel

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,9 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Menu");
+            frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(300, 200);
+            frame.setSize(400, 400);
             frame.setLayout(new GridLayout(6, 1));
 
             JButton artistaButton = new JButton("Criar Artista");
